b0ad7ab663517fef1b98a1bb593b6d0566feaeb4
Jack Brennan <jtbrenna@uvm.edu>
2014-04-28 18:41:00 -0400

    gave welcome.php bootstrap style

--- bootstrap-peter/welcome.php
+++ bootstrap-peter/welcome.php
@@ -16,8 +16,25 @@ $user = unserialize($_SESSION['user']);
 <html>
 <head>
 	<title>Welcome <?php echo $user->username; ?></title>
+	<link rel="stylesheet" href="css/bootstrap.css"  type="text/css">
 </head>
 <body>
-	Hey there, <?php echo $user->username; ?>. You've been registered and logged in. Welcome! <a href="logout.php">Log Out</a> | <a href="index.php">Return to Homepage</a>
+	<!--This is the bootstrap framework for our site, subject to change, obvioiusly.  replace hrefs leading to "#" with their actual targets as we build them"-->
+
+	<div class="container">
+	<h1><a href="index.php">Blog Diggity!</a></h1>
+
+	<?php include 'includes/nav.php'; ?>
+	
+	<div class="hero-unit">
+    	<h2>Welcome to Blog Diggity</h2>
+ 		<p>Hey there, <?php echo $user->username; ?>. You've been registered and logged in. Welcome! <a href="logout.php">Log Out</a> | <a href="index.php">Return to Homepage</a></p>
+ 
+ 	</div>
+	</div>	<!--Main container-->
+
+		<!--Import jquery and bootstrap-->
+		<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+		<script src="js/bootstrap.js"></script>
 </body>
 </html>
