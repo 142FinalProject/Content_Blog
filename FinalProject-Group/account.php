aeb7bc553edda589a7ee830d6ad92d98de97abed
Jack Brennan <jtbrenna@uvm.edu>
2014-05-06 11:55:42 -0400

    added words to post.php

--- FinalProject-Group/account.php
+++ FinalProject-Group/account.php
@@ -88,13 +88,13 @@ require_once 'includes/global.inc.php';
          		$result = substr($row["link"], 0, 4);
          		if ($result == "http"){
 					print("<h4><a href='" . $row["link"] . "'>" . $row["title"] . "</a></h4>");
-					print("</tr>");
-					print("</td>");
+
          		}else{
          			print("<h4><a href='http://" . $row["link"] . "'>" . $row["title"] . "</a></h4>");
-         			print("</tr>");
-					print("</td>");
+         			
          		}
+         		print("</tr>");
+				print("</td>");
 	  		}
 	  		print("</table>");
 	  		print("</div>");
